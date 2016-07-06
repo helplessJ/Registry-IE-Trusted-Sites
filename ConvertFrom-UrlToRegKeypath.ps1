6b6cdb06974209b00d587339e4f750f778bfa390
helplessJ <helplessJ@users.noreply.github.com>
2016-07-05 21:26:27 -0700

    Add files via upload

---
+++ ConvertFrom-UrlToRegKeypath.ps1
@@ -0,0 +1,59 @@
+﻿Function ConvertFrom-UrlToRegistryUrl {
+
+    
+    [cmdletbinding()]
+    param(
+            [Parameter(Mandatory=$true)]
+            [string]$rawUrl
+         )
+         
+    Switch -Regex ($rawUrl){
+        '^\*' {
+            IF(($rawUrl -split '\.').Count -eq 3){
+                $rawUrl
+            }else{
+                $wantedEleCount = ($rawUrl -split '\.').count -1
+                $allDotSub = (($rawUrl -split '\.', 3)[0..1]) -join '.'
+                $remDotSub = ($rawUrl -split '\.', 3)[2]
+                $grpA = ($remDotSub -split '\.',$wantedEleCount)[0..($wantedEleCount - 2)]
+                $loop = $wantedEleCount - 2
+                $revGrpA = @()
+                if($grpA.count -ne 2){
+                    foreach ($ele in $grpA)
+                    {
+                        $revGrpA += $grpA[$loop]
+                        $loop--
+                    }
+                }
+                $baseUrl = ($rawUrl -split '\.',$wantedEleCount)[$wantedEleCount -1]
+                $allEles = @()
+                $allEles += $baseUrl
+                $allEles += $revGrpA
+                $allEles += $allDotSub
+                $newRegUrl = $allEles -join '\'
+                $newRegUrl
+            }
+        }
+        default {
+            IF(($rawUrl -split '\.').count -eq 2){
+                $rawUrl
+            } else{
+                $wantedEleCount = ($rawUrl -split '\.').count -1
+                $grpA = ($rawUrl -split '\.',$wantedEleCount)[0..($wantedEleCount - 2)]
+                $loop = $wantedEleCount - 2
+                $revGrpA = @()
+                    foreach ($ele in $grpA)
+                {
+                    $revGrpA += $grpA[$loop]
+                    $loop--
+                }
+                $baseUrl = ($rawUrl -split '\.',$wantedEleCount)[$wantedEleCount -1]
+                $allEles = @()
+                $allEles += $baseUrl
+                $allEles += $revGrpA
+                $newRegUrl = $allEles -join '\'
+                $newRegUrl
+            } #else
+        } #default
+    } #switch
+} #function
---
+++ ConvertFrom-UrlToRegKeypath.tests.ps1
@@ -0,0 +1,27 @@
+﻿# this is a Pester test file
+
+#region Further Reading
+# http://www.powershellmagazine.com/2014/03/27/testing-your-powershell-scripts-with-pester-assertions-and-more/
+#endregion
+#region LoadScript
+# load the script file into memory
+# attention: make sure the script only contains function definitions
+# and no active code. The entire script will be executed to load
+# all functions into memory
+. ($PSCommandPath -replace '\.tests\.ps1$', '.ps1')
+#endregion
+Describe 'ConvertFrom-UrlToRegistryUrl' {
+    It 'Leaves "*.microsoft.com" domains as-is'   {
+        ConvertFrom-UrlToRegistryUrl -rawUrl '*.microsoft.com' | Should Be '*.microsoft.com'
+    }
+    It 'Converts "www.microsoft.com" domain to "microsoft.com\www"' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl 'www.microsoft.com' | Should Be 'microsoft.com\www'
+    }
+    It 'Converts "*.sub.fanniemae.org" to "fanniemae.org\*.sub' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl '*.sub.fanniemae.org' | Should Be 'fanniemae.org\*.sub'
+    }
+    It 'Converts "*.ridiculous.subDomain.example.com" to "example.com\*.ridiculous.subDomain"' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl '*.ridiculous.subDomain.example.com' | Should Be 'example.com\*.ridiculous.subDomain'
+    }
+}
+
