e0e7ded313e8c853881ac719e61fb93fd6163518
helplessJ <helplessJ@users.noreply.github.com>
2016-07-08 11:15:08 -0700

    Refactored. Additional Tests. Bug fix
    Bug fix domains with more than 1 sub domain are now handled correctly. Example: ridiculous.long.domain.microsoft.com

--- ConvertFrom-UrlToRegKeypath.ps1
+++ ConvertFrom-UrlToRegKeypath.ps1
@@ -1,59 +1,91 @@
-﻿Function ConvertFrom-UrlToRegistryUrl {
-
+﻿Function ConvertTo-RegKeyFormat {
     
-    [cmdletbinding()]
-    param(
-            [Parameter(Mandatory=$true)]
-            [string]$rawUrl
-         )
-         
-    Switch -Regex ($rawUrl){
-        '^\*' {
-            IF(($rawUrl -split '\.').Count -eq 3){
-                $rawUrl
-            }else{
-                $wantedEleCount = ($rawUrl -split '\.').count -1
-                $allDotSub = (($rawUrl -split '\.', 3)[0..1]) -join '.'
-                $remDotSub = ($rawUrl -split '\.', 3)[2]
-                $grpA = ($remDotSub -split '\.',$wantedEleCount)[0..($wantedEleCount - 2)]
-                $loop = $wantedEleCount - 2
-                $revGrpA = @()
-                if($grpA.count -ne 2){
-                    foreach ($ele in $grpA)
-                    {
-                        $revGrpA += $grpA[$loop]
-                        $loop--
-                    }
-                }
-                $baseUrl = ($rawUrl -split '\.',$wantedEleCount)[$wantedEleCount -1]
-                $allEles = @()
-                $allEles += $baseUrl
-                $allEles += $revGrpA
-                $allEles += $allDotSub
-                $newRegUrl = $allEles -join '\'
-                $newRegUrl
-            }
-        }
-        default {
-            IF(($rawUrl -split '\.').count -eq 2){
-                $rawUrl
-            } else{
-                $wantedEleCount = ($rawUrl -split '\.').count -1
-                $grpA = ($rawUrl -split '\.',$wantedEleCount)[0..($wantedEleCount - 2)]
-                $loop = $wantedEleCount - 2
-                $revGrpA = @()
-                    foreach ($ele in $grpA)
-                {
-                    $revGrpA += $grpA[$loop]
-                    $loop--
-                }
-                $baseUrl = ($rawUrl -split '\.',$wantedEleCount)[$wantedEleCount -1]
-                $allEles = @()
-                $allEles += $baseUrl
-                $allEles += $revGrpA
-                $newRegUrl = $allEles -join '\'
-                $newRegUrl
-            } #else
-        } #default
-    } #switch
-} #function
+    
+    param($rawUrl)
+    
+    $urlllElements = ($rawUrl -split '\.').count
+    $baseDomain = ($rawUrl -split '\.',($urlllElements -1))[($urlllElements - 2)..($urlllElements - 1)]
+    $urlllSubDomains = ($rawUrl -split '\.')[0..($urlllElements -3)]
+    $urlllSubDomains = $urlllSubDomains -join '.'
+    
+    $recontructedUrl = @()
+    $recontructedUrl += $baseDomain
+    $recontructedUrl += $urlllSubDomains
+    $newRegUrl = $recontructedUrl -join '\'
+    $newRegUrl
+}
+
+
+Function Assert-Url {
+
+    param($rawUrl='error')
+    
+    $url = $rawUrl
+    $approvedDomainSuffixes = '.com|.net|.org'
+    
+    if($url[($url.Length -4)..($url.Length - 1)] -join '' -match $approvedDomainSuffixes){
+        $true
+    } else {
+        Throw 'The supplied does not end with .com ; .net ; or .org'; break
+    }
+
+}
+
+function Convert-UrlToRegistryPath
+{
+<#
+	.SYNOPSIS
+		Send a web domain url and an IE trusted site domain key is returned.
+	
+	.DESCRIPTION
+		For use with IE trusted sites management.  This function will return the key equivelent for a given url.  RawUrl must not contain http or https and must end in .com .net or .org.  To add domain suffixes, modify $approvedDomainSuffixes in Assert-Url function.  This does not create registry keys, simply the key path that you would append the the HKLM or HKCU registry path.
+	
+	.PARAMETER rawUrl
+		The url to convert to a registry key name.
+	
+	.NOTES
+		Additional information about the function.
+#>
+	
+	[CmdletBinding()]
+	[OutputType([string])]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateScript({ Assert-Url -rawUrl $_ })]
+		[string]$rawUrl
+	)
+	
+	$startsWithAsterisk = '^\*'
+	$startsWithHttp = '^http'
+	
+	Switch -Regex ($rawUrl)
+	{
+		
+		$startsWithHttp { Throw 'HTTP and HTTPS are not allowed'; break }
+		
+		$startsWithAsterisk {
+			IF (($rawUrl -split '\.').Count -eq 3)
+			{
+				$rawUrl
+			}
+			else
+			{
+				ConvertTo-RegKeyFormat -rawUrl $rawUrl
+			}
+			break
+		}
+		
+		default
+		{
+			IF (($rawUrl -split '\.').count -eq 2)
+			{
+				$rawUrl
+			}
+			else
+			{
+				ConvertTo-RegKeyFormat -rawUrl $rawUrl
+			}
+		}
+	}
+}
--- ConvertFrom-UrlToRegKeypath.tests.ps1
+++ ConvertFrom-UrlToRegKeypath.tests.ps1
@@ -10,18 +10,50 @@
 # all functions into memory
 . ($PSCommandPath -replace '\.tests\.ps1$', '.ps1')
 #endregion
+
 Describe 'ConvertFrom-UrlToRegistryUrl' {
-    It 'Leaves "*.microsoft.com" domains as-is'   {
+    
+    It 'Throws an exception if url begins with http' {
+        {ConvertFrom-UrlToRegistryUrl -rawUrl 'http://www.microsoft.com'} | Should Throw 'HTTP and HTTPS are not allowed'
+    }
+    It 'Will leave full domain "*.microsoft.com" as-is'   {
         ConvertFrom-UrlToRegistryUrl -rawUrl '*.microsoft.com' | Should Be '*.microsoft.com'
     }
-    It 'Converts "www.microsoft.com" domain to "microsoft.com\www"' {
-        ConvertFrom-UrlToRegistryUrl -rawUrl 'www.microsoft.com' | Should Be 'microsoft.com\www'
+    It 'Will leave single domain "microsoft.com" as-is' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl 'microsoft.com' | Should Be 'microsoft.com'
     }
     It 'Converts "*.sub.fanniemae.org" to "fanniemae.org\*.sub' {
         ConvertFrom-UrlToRegistryUrl -rawUrl '*.sub.fanniemae.org' | Should Be 'fanniemae.org\*.sub'
     }
     It 'Converts "*.ridiculous.subDomain.example.com" to "example.com\*.ridiculous.subDomain"' {
-        ConvertFrom-UrlToRegistryUrl -rawUrl '*.ridiculous.subDomain.example.com' | Should Be 'example.com\*.ridiculous.subDomain'
+        ConvertFrom-UrlToRegistryUrl -rawUrl '*.ridiculous.subDomain.example.com' |
+          Should Be 'example.com\*.ridiculous.subDomain'
+    }
+    It 'Converts "www.microsoft.com" to "microsoft.com\www"' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl 'www.microsoft.com' | Should Be 'microsoft.com\www'
+    }
+    It 'Converts "sub.microsoft.com" to "microsoft.com\sub"' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl 'sub.microsoft.com' | Should Be 'microsoft.com\sub'
+    }
+    It 'Converts "ridiculous.sub.microsoft.com" to "microsoft.com\ridiculous.sub' {
+        ConvertFrom-UrlToRegistryUrl -rawUrl 'ridiculous.sub.microsoft.com' |
+          Should Be 'microsoft.com\ridiculous.sub'
     }
 }
 
+Describe 'Assert-Url' {
+
+    Context 'Running without arguments'   {
+        It 'Throws error if run without arguments with errors' {
+            { Assert-Url } | Should Throw
+        }
+        It 'Throws error if URL does not end with approved suffix (.com/.net/.org)' {
+             { Assert-Url -rawUrl 'bad.xample' } | Should Throw
+        }
+        It 'Returns $true if proper input is given'{
+			Assert-Url -rawUrl 'example.com' | Should Be $true
+			Assert-Url -rawUrl 'http://www.example.com' | Should Be $true
+			Assert-Url -rawUrl 'login.microsoft.com' | Should Be $true
+        }
+    }
+}
